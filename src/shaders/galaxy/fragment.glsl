8ec3cb64bc896402d1244ade15e5ae8db31c0b94
Rebecca Young <moonbladez@hotmail.com>
2023-09-17 11:28:46 +0200

    readded color#

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,5 @@
+varying vec3 vColor;
+
 void main() {
     // //disc pattern
     // float strength = distance(gl_PointCoord, vec2(0.5));
@@ -15,9 +17,9 @@ void main() {
     strength = pow(strength, 10.0);
 
 
+    vec3 color = mix(vec3(0.0), vColor, strength);
 
+    gl_FragColor = vec4(color, 1.0);
 
-    gl_FragColor = vec4(vec3(strength), 1.0);
-
-
+   
 }
